b6777ea12631577697bf0672a9e937c799fea0ad
buiminhhai1 <ngovietduc20088@gmail.com>
2019-12-25 14:47:45 +0700

    api update complain

--- backend/backend-user/components/complain/controller/complainController.js
+++ backend/backend-user/components/complain/controller/complainController.js
@@ -35,50 +35,31 @@ exports.createComplain = async (req, res, next) => {
   }
 };
 
-// exports.updateContract = async (req, res, next) => {
-//   const { _id, idsub, review, score, status } = req.body;
-//   try {
-//     const contract = await ContractModel.findById(_id);
-//     const idTeach = contract.teacher.userId;
-//     const idStudent = contract.student.userId;
-//     const teacher = await UserModel.findById(idTeach);
-//     const student = await UserModel.findById(idStudent);
-//     if (teacher && student) {
-//       const updateTeacher = teacher.contracts.id(_id);
-//       updateTeacher.review = review;
-//       updateTeacher.score = score;
-//       updateTeacher.status = status;
-//       // teacher.contracts.id(_id) = updateTeacher;
-//       const updateStudent = student.contracts.id(_id);
-//       updateStudent.status = status;
-//       updateStudent.review = review;
-//       updateStudent.score = score;
-//       await teacher.save();
-//       await student.save();
-//     } else {
-//       return res.json({
-//         message: 'update contract has failed because user not fount'
-//       });
-//     }
 
-//     if (contract) {
-//       contract.status = status;
-//       contract.review = review;
-//       contract.score = score;
-//       await contract.save();
-//       return res.json({
-//         contract,
-//         message: 'update contract has success'
-//       });
-//     }
-//     return res.json({
-//       message: 'update contract has fail',
-//       error: 'update contract has failed'
-//     });
-//   } catch (err) {
-//     return res.json({
-//       error: err,
-//       message: 'update has failed!'
-//     });
-//   }
-// };
+exports.updateComplain = async (req, res, next) => {
+  const {
+    _id,
+    content
+  } = req.body;
+
+  try {
+    const updated = await ComplainModel.findById(_id);
+    if (updated) {
+      updated.content = content;
+      const result = await updated.save();
+      return res.json({
+        complain: result,
+        message: 'Update complain success!'
+      });
+    }
+    return res.json({
+      error: 'Update complain has failed!',
+      message: 'Update complain has fail'
+    });
+  } catch (err) {
+    return res.json({
+      error: err,
+      message: 'Something went wrong!'
+    });
+  }
+};
--- backend/backend-user/components/complain/router/complainRouter.js
+++ backend/backend-user/components/complain/router/complainRouter.js
@@ -8,11 +8,8 @@ router.post('/createComplain', passport.authenticate('jwt', {
   session: false
 }), complainController.createComplain);
 
-// router.put('/updateComplain', passport.authenticate('jwt', {
-//   session: false
-// }), complainController.updateComplain);
+router.put('/updateComplain', passport.authenticate('jwt', {
+  session: false
+}), complainController.updateComplain);
 
-// router.get('/getDetailComplain/:idComplain', passport.authenticate('jwt', {
-//   session: false
-// }), complainController.getDetailComplain);
 module.exports = router;
