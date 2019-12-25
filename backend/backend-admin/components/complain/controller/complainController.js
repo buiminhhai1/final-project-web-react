86d2f28a49d0998483a59b0762e7679d3860d39b
buiminhhai1 <ngovietduc20088@gmail.com>
2019-12-25 16:20:02 +0700

    complain with admin getList

---
+++ backend/backend-admin/components/complain/controller/complainController.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+const ComplainModel = require('../model/complainModel');
+
+exports.getListComplain = async (req, res, next) => {
+  try {
+    const complains = await ComplainModel.find({});
+    return res.json({
+      complains,
+      message: 'Get List Complain success'
+    });
+  } catch (err) {
+    return res.json({
+      error: err,
+      message: 'Something went wrong!'
+    });
+  }
+};
+
+// exports.updateComplain = async (req, res, next) => {
+//   const {
+//     _id,
+//     content
+//   } = req.body;
+
+//   try {
+//     const updated = await ComplainModel.findById(_id);
+//     if (updated) {
+//       updated.content = content;
+//       const result = await updated.save();
+//       return res.json({
+//         complain: result,
+//         message: 'Update complain success!'
+//       });
+//     }
+//     return res.json({
+//       error: 'Update complain has failed!',
+//       message: 'Update complain has fail'
+//     });
+//   } catch (err) {
+//     return res.json({
+//       error: err,
+//       message: 'Something went wrong!'
+//     });
+//   }
+// };
---
+++ backend/backend-admin/components/complain/router/complainRouter.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const passport = require('passport');
+
+const router = express.Router();
+const complainController = require('../controller/complainController');
+
+router.get('/getListComplain', passport.authenticate('jwt', {
+  session: false
+}), complainController.getListComplain);
+
+// router.put('/updateComplain', passport.authenticate('jwt', {
+//   session: false
+// }), complainController.updateComplain);
+
+module.exports = router;
