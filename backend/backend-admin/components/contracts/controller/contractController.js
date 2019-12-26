732f7377e98c87efe142a701bb7cc7fa726e0e17
dinhhn2000 <dinhhongoc@gmail.com>
2019-12-26 09:24:13 +0700

    + Statitics by day

--- backend/backend-admin/components/contracts/controller/contractController.js
+++ backend/backend-admin/components/contracts/controller/contractController.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+// const mongoose = require('mongoose');
 const ContractModel = require('../model/contractModel');
 
 exports.getListContract = async (req, res, next) => {
@@ -15,3 +15,47 @@ exports.getListContract = async (req, res, next) => {
     });
   }
 };
+
+exports.statiticsByDay = async (req, res, next) => {
+  try {
+    let startDay = Date.parse(req.query.startDay);
+    let endDay = Date.parse(req.query.endDay);
+    // (endDay - startDay) / 86400000;
+    let days = [];
+    while (endDay >= startDay) {
+      days.push(startDay);
+      startDay += 86400000;
+    }
+
+    let contracts = await ContractModel.find({});
+    contracts = contracts.filter(contract => contract.status === 2);
+    contracts.forEach(contract => {
+      contract.to = Date.parse(contract.to);
+    })
+
+    let result = [];
+    days.forEach(day => {
+      let total = 0;
+      contracts.forEach(contract => {
+        if (parseInt(contract.to) === day) {
+          total += contract.hourRate * contract.totalHourCommit;
+        }
+      })
+      result.push({
+        day,
+        total
+      });
+    })
+
+    return res.json({
+      statitics: result,
+    })
+  }
+  catch (err) {
+    console.log(err);
+    return res.json({
+      error: err,
+      message: 'Cannot statitics'
+    });
+  }
+}
